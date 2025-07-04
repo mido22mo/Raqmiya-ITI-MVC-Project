6665dc450645ca3305ccb16085723379285ea44e
mahmod7521 <mahmoudemad7521@gmail.com>
2025-07-04 19:55:57 +0300

    test

--- obj/Debug/net8.0/ITI-Raqmiya-MVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/ITI-Raqmiya-MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITI-Raqmiya-MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6102f0d78c936496ca27d7e1a19d41c1d920a829")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITI-Raqmiya-MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITI-Raqmiya-MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
